4a7adad67c9b4ac6fd226e9a74d0eaea6be4b98b
Kirill013 <korelin-k1@mail.ru>
2025-05-05 01:26:45 +0300

    build & config

---
+++ build/CMakeFiles/Monitor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Monitor.dir/Monitor.cpp.o"
+  "CMakeFiles/Monitor.dir/Monitor.cpp.o.d"
+  "Monitor"
+  "Monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
